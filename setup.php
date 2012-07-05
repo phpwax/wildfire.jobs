308e078a6b16619b9f52d3b198319bf097fb1d0e
charlesmarshall <cm56marshall@googlemail.com>
2012-07-05 11:11:23 +0100

    add in join on content model the to the questions, this is where they will be created

---
+++ setup.php
@@ -0,0 +1,16 @@
+<?
+//hook in to the content model and add a join
+
+if(!defined("CONTENT_MODEL")){
+  $con = new ApplicationController(false, false);
+  define("CONTENT_MODEL", $con->cms_content_class);
+}
+
+
+WaxEvent::add(CONTENT_MODEL.".setup", function(){
+  $model = WaxEvent::data();
+  if($model->columns['forms']) unset($model->columns['forms']);
+  $model->define("fields", "ManyToManyField", array('scaffold'=>true, 'target_model'=>'Question', 'group'=>'Questions', 'editable'=>true));
+});
+
+?>
