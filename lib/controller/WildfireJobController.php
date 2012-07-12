bab7a6bc96b8cfced49a8f915c4339773d7071d1
charlesmarshall <cm56marshall@googlemail.com>
2012-07-12 13:30:54 +0100

    tidy up the code into functions for easier overwritting. fix the code to do the active form checking

--- lib/controller/WildfireJobController.php
+++ lib/controller/WildfireJobController.php
@@ -3,45 +3,66 @@ class WildfireJobController extends ApplicationController{
 
   public $answer_forms = array(); //array of forms that need to be answered
   public $active_form = 0;
+  public $setform;
   //from a job id, generate the form
   public function __job(){
     $content_class = $this->cms_content_class;
     $content = new $content_class($this->job_primval);
-    //track this via a cookie
-    if(!$this->session_id){
-      $this->session_id = Session::get_hash();
-      Cookie::set("hashed", $this->session_id);
-    }
-    //get / set the application model
-    $application = new Application($this->application_primval);
-    if(!$this->application_primval || !($application->primval == $this->application_primval) ){
-      $saved = $application->update_attributes(array('session'=>$this->session_id,$content->table."_".$content->primary_key=>$this->job_primval));
-      Session::set('application', $saved->primval);
-      $this->application_primval = $saved->primval;
-    }
-    //work this stuff out only if there are questions to answer!
-    if($content && $content->primval && ($questions = $content->fields) && $questions->count()){
-      $this->questions = $questions;
-      foreach($questions->order('field_group ASC')->all() as $q){
-        $a = $this->setup_answer($q);
-        $form = new WaxForm($a);
 
-        $this->answer_forms[] = $form;
-      }
+    $this->session_id = $this->session_cookie();
+    $this->application_primval = $this->get_application($content, $this->job_primval, Session::get('application'), $this->session_id);
+
+    $this->answer_forms = $this->get_forms($content);
+
+    $cookie = $this->job_primval."-current";
+
+    //if form is being posted & its within range
+    $posted = Request::param('_form');
+    if($posted !== null && ($to_save = $this->answer_forms[$posted]) && ($saved = $to_save->save())){
+      $application->answers = $saved;
+      $this->answer_forms[$posted] = new WaxForm($saved);
     }
+    //this allows a manual override of the active form
+    if($this->setform) $this->active_form = $this->setform;
+    else if($posted !== null) $this->active_form = $posted + 1;
+
     //check existsing saves to increase the position counter
     foreach($this->answer_forms as $k=>$form){
       if($form->handler->bound_to_model && $form->handler->bound_to_model->primval) $this->active_form = $k+1;
     }
-    //handle saves
-    $posted = Request::param("_form");
-    if(($posted !== false) && ($form = $this->answer_forms[$posted])){
-      $saved = $form->save();
-      $application->answers = $saved;
-      $this->answer_forms[$posted] = new WaxForm($saved);
-      $this->active_form = $posted + 1;
-    }
+    Cookie::set($this->job_primval."-current", $this->active_form);
+  }
 
+  protected function session_cookie(){
+    if(!$session_id = Cookie::get("hashed")){
+      $session_id = Session::get_hash();
+      Cookie::set("hashed", $session_id);
+    }
+    return Cookie::get("hashed");
+  }
+
+  protected function get_forms($content){
+    $answer_forms = array();
+    if($content && $content->primval && ($questions = $content->fields) && $questions->count()){
+      foreach($questions->order('`order` ASC')->all() as $q){
+        $a = $this->setup_answer($q);
+        $form = new WaxForm($a);
+        $answer_forms[] = $form;
+      }
+    }
+    return $answer_forms;
+  }
+
+  protected function get_application($content, $job_primval, $application_primval, $session_id){
+    //get / set the application model
+    $application = new Application($application_primval);
+    if(!$this->application_primval || !($application->primval == $application_primval) ){
+      $saved = $application->update_attributes(array('session'=>$session_id, $content->table."_".$content->primary_key=>$job_primval));
+      Session::set('application', $saved->primval);
+      $application_primval = $saved->primval;
+
+    }
+    return Session::get('application');
   }
 
   protected function setup_answer($q){
@@ -60,6 +81,8 @@ class WildfireJobController extends ApplicationController{
       $a->question_subtext = $q->subtext;
     }
     $a->columns['question_subtext'][1]['widget'] = $a->columns['question_text'][1]['widget'] = "HiddenInput";
+    //make it a required field
+    if($q->required == 1 || $q->required == 2) $a->columns['answer'][1]['required'] = true;
     //change the answer
     $a->columns['answer'][1]['widget'] = $q->field_type;
     if($q->choices){
