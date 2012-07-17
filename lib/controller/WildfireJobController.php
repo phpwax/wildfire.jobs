81233229dccba9bb9c0c04cf0b10b3c681a5046b
charlesmarshall <cm56marshall@googlemail.com>
2012-07-17 12:59:12 +0100

    add in hooks to find if the form is completed, deadends etc

--- lib/controller/WildfireJobController.php
+++ lib/controller/WildfireJobController.php
@@ -5,6 +5,12 @@ class WildfireJobController extends ApplicationController{
   public $active_form = 0;
   public $total_questions = 0;
   public $setform;
+  public $posted_form = false;
+  public $saved_forms = array();
+  public $error_forms = array();
+  public $deadend = false;
+  public $completed = false;
+
   //from a job id, generate the form
   public function __job(){
     WaxEvent::run("job.start", $this);
@@ -14,23 +20,41 @@ class WildfireJobController extends ApplicationController{
     $this->session_id = $this->session_cookie();
     WaxEvent::run("job.session", $this);
     $this->application_primval = $this->get_application($content, $this->job_primval, Session::get('application'), $this->session_id);
+    $application = new Application($this->application_primval);
     WaxEvent::run("job.application", $this);
     $this->answer_forms = $this->get_forms($content);
     WaxEvent::run("job.answer_forms", $this);
     $cookie = $this->job_primval."-current";
 
     //if form is being posted & its within range
-    $posted = Request::param('_form');
-    if($posted !== null && ($to_save = $this->answer_forms[$posted]) && ($saved = $to_save->save())){
+    $this->posted_form = $posted = Request::param('_form');
+    $to_save = $this->answer_forms[$posted];
+    if($posted !== null && $to_save && ($saved = $to_save->save())){
       $application->answers = $saved;
       $this->answer_forms[$posted] = new WaxForm($saved);
+      $this->saved_forms[$posted] = $saved;
     }
+    //check for errors - empty fields that are require
+    if($to_save && ($errors = $to_save->errors())) $this->error_forms[$posted] = $errors;
+    //check for deadends
+    if($dead = $this->deadend($to_save)){
+      $this->deadend = true;
+      $application->update_attributes(array('deadend'=>1, 'completed'=>1, 'date_completed'=>date("Y-m-d H:i:s")));
+    }
+
     WaxEvent::run("job.save", $this);
     WaxEvent::run("job.active_form.before", $this);
     //check existsing saves to increase the position counter
     foreach($this->answer_forms as $k=>$form){
-      if($form->handler->bound_to_model && $form->handler->bound_to_model->primval) $this->active_form = $k+1;
+      if($form->handler->bound_to_model && $form->handler->bound_to_model->primval){
+        $this->active_form = $k+1;
+        if($this->active_form == count($this->answer_forms)){
+          $application->update_attributes(array('completed'=>1));
+          $this->completed = false;
+        }
+      }
     }
+
     WaxEvent::run("job.active_form.questions", $this);
     //this allows a manual override of the active form
     if($this->setform !== null) $this->active_form = $this->setform;
@@ -38,6 +62,18 @@ class WildfireJobController extends ApplicationController{
     WaxEvent::run("job.active_form.after", $this);
     Cookie::set($this->job_primval."-current", $this->active_form);
     WaxEvent::run("job.end", $this);
+  }
+
+  //if any dead end question has been answered incorrectly, then flag as a deadend
+  protected function deadend($form){
+    $test = $form->handler->bound_to_model;
+    foreach($test->columns as $col){
+      $setup = $col[1];
+      $choice = array_shift($setup['choices']);
+      //answer has been set, its a deadend and doesnt match
+      if($test->answer && $col[1]['deadend'] && $test->answer != $choice) return true;
+    }
+    return false;
   }
 
   protected function session_cookie(){
@@ -70,7 +106,7 @@ class WildfireJobController extends ApplicationController{
       $saved = $application->update_attributes(array('session'=>$session_id, $content->table."_".$content->primary_key=>$job_primval));
       Session::set('application', $saved->primval);
       $application_primval = $saved->primval;
-
+      return $application_primval;
     }
     return Session::get('application');
   }
@@ -93,7 +129,7 @@ class WildfireJobController extends ApplicationController{
     $a->columns['question_subtext'][1]['widget'] = $a->columns['question_text'][1]['widget'] = "HiddenInput";
     //make it a required field
     if($q->required == 1 || $q->required == 2) $a->columns['answer'][1]['required'] = true;
-    if($q->required == 2) $a->columns['answer'][1]['deadend'] = true;
+    if($q->required == 2) $a->columns['answer'][1]['deadend'] = "deadend";
     //change the answer
     $a->columns['answer'][1]['widget'] = $q->field_type;
     if($q->choices){
