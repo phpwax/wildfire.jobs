ee213ff40c98b694e4905a6d9f43d2f08f324d0b
charlesmarshall <cm56marshall@googlemail.com>
2012-07-19 14:56:01 +0100

    new function to compare answers to whats need in the application

--- lib/model/Application.php
+++ lib/model/Application.php
@@ -21,5 +21,26 @@ class Application extends WaxModel{
     if(!$this->date_start) $this->date_start = date("Y-m-d H:i:s");
     if(!$this->completed) $this->completed = 0;
   }
+
+  public function complete($job){
+    if($this->deadend) return false;
+    $complete = 1;
+    //answers
+    $answered = array();
+    foreach($this->answers as $a) $answered[$a->question_id] = trim($a->answer);
+    //no answers
+    if(!count($answered)) return false;
+    //check whats been answered
+    foreach($job->fields as $field){
+      //if the field is required then must be in the array
+      if($field->required && !$answered[$field->primval]) return false;
+      //if the field is a deadend then the value must match first
+      $c = explode("\n", $field->choices);
+      //get the first one
+      $should_be = trim(array_shift($c));
+      if($field->required == 2 && $should_be != $answered[$field->primval]) return false;
+    }
+    return $complete;
+  }
 }
 ?>
