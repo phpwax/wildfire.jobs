94a902aff80f81e42d2d3936bab5d7abaa3b07c3
charlesmarshall <cm56marshall@googlemail.com>
2012-07-20 15:34:54 +0100

    comment out some filters, need to fix the model being passed in to filters

--- lib/controller/CMSAnswersController.php
+++ lib/controller/CMSAnswersController.php
@@ -9,7 +9,12 @@ class CMSAnswersController extends AdminComponent{
     public $sortable = false;
     public $per_page = 20;
     public $limit_revisions = 20; //limit revisions as it may cause problems
-    public $filter_fields=array();
+    // public $filter_fields=array(
+    //                         'job' => array('columns'=>array('domain_content_id'), 'partial'=>'_filters_select'),
+    //                         'submitted_at' => array('columns'=>array('submitted_at'), 'partial'=>"_filters_date", 'fuzzy_right'=>true),
+    //                         'completed' => array('columns'=>array('completed'), 'partial'=>"_filters_status"),
+    //                         'deadend' => array('columns'=>array('deadend'), 'partial'=>"_filters_status")
+    //                       );
     public $autosave = false;
 
 }
