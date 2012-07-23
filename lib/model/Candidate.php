e74b2a204e0ec48ddcc95be9104d6ce34f88b74e
charlesmarshall <cm56marshall@googlemail.com>
2012-07-23 12:54:48 +0100

    start of candidate model

---
+++ lib/model/Candidate.php
@@ -0,0 +1,13 @@
+<?
+class Candidate extends WaxModel{
+
+  public function setup(){
+    parent::setup();
+    $this->define("first_name", "CharField");
+    $this->define("last_name", "CharField");
+    $this->define("date_of_birth", "DateTimeField");
+
+  }
+
+}
+?>
