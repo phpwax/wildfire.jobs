dbd6aa000208a699c8725d4b03df3982e75d7f3f
charlesmarshall <cm56marshall@googlemail.com>
2012-07-23 16:54:34 +0100

    add phone number

--- lib/model/Candidate.php
+++ lib/model/Candidate.php
@@ -6,7 +6,7 @@ class Candidate extends WaxModel{
     $this->define("first_name", "CharField");
     $this->define("last_name", "CharField");
     $this->define("date_of_birth", "DateTimeField");
-
+    $this->define("telephone", "CharField");
   }
 
 }
