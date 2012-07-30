db02691f64a9fd24cea1ce4d24fbacbddbf745cc
charlesmarshall <cm56marshall@googlemail.com>
2012-07-30 09:38:01 +0100

    add position to scaffold

--- lib/model/Application.php
+++ lib/model/Application.php
@@ -4,7 +4,7 @@ class Application extends WaxModel{
   public function setup(){
     parent::setup();
     $this->define("answers", "HasManyField", array('target_model'=>'Answer', 'group'=>'answers', 'editable'=>true));
-    $this->define("job", "ForeignKey", array('target_model'=>CONTENT_MODEL, 'scaffold'=>true, 'export'=>true, 'group'=>'relationships'));
+    $this->define("job", "ForeignKey", array('target_model'=>CONTENT_MODEL, 'scaffold'=>true, 'export'=>true, 'group'=>'relationships', 'scaffold'=>true));
     $this->define("date_start", "DateTimeField");
     $this->define("date_completed", "DateTimeField", array('scaffold'=>true, 'export'=>true));
     $this->define("session", "CharField", array('editable'=>false));
