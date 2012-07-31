0edaf1f2a4793b279ea9c557ab4a25887635e089
charlesmarshall <cm56marshall@googlemail.com>
2012-07-31 13:18:26 +0100

    new filters and swap to extends application cms controller for the export options

--- lib/controller/CMSMeetingController.php
+++ lib/controller/CMSMeetingController.php
@@ -1,5 +1,5 @@
 <?
-class CMSMeetingController extends AdminComponent{
+class CMSMeetingController extends CMSApplicantController{
 
     public $dashboard = false;
     public $module_name = "meeting";
@@ -10,9 +10,13 @@ class CMSMeetingController extends AdminComponent{
     public $per_page = 20;
     public $limit_revisions = 20; //limit revisions as it may cause problems
     public $filter_fields=array(
-                            'job' => array('columns'=>array('domain_content_id'), 'partial'=>'_filters_select')
+                            'job' => array('columns'=>array('domain_content_id'), 'partial'=>'_filters_select'),
+                            'date_start' => array('columns'=>array('date_start'), 'partial'=>"_filters_date", 'fuzzy_right'=>true)
                           );
     public $autosave = false;
+    public $list_options = array(
+                            array('form_name'=>'export_pdf', 'form_value'=>'Export as PDF', 'class'=>'revision')
+                          );
 
 }
 ?>
