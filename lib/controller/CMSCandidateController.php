b05e0a570d776b4b0f7b97c8676f43c658e84946
charlesmarshall <cm56marshall@googlemail.com>
2012-07-31 13:17:24 +0100

    join candidates to meetings

--- lib/controller/CMSCandidateController.php
+++ lib/controller/CMSCandidateController.php
@@ -11,6 +11,7 @@ class CMSCandidateController extends CMSApplicantController{
 
     public $filter_fields=array(
                               'job' => array('columns'=>array('job_id'), 'partial'=>'_filters_select'),
+                              'date_created' => array('columns'=>array('date_created'), 'partial'=>"_filters_date", 'fuzzy_right'=>true)
                               );
     public $autosave = false;
     public $list_options = array(
@@ -19,7 +20,6 @@ class CMSCandidateController extends CMSApplicantController{
                             array('form_name'=>'export_pdf', 'form_value'=>'Export as PDF', 'class'=>'revision'),
                             array('form_name'=>'arrange_meeting', 'form_value'=>'Arrange meeting', 'class'=>'revision')
                           );
-
 
     protected function events(){
       parent::events();
@@ -32,14 +32,20 @@ class CMSCandidateController extends CMSApplicantController{
         //if an existing id is posted then join the candidates to it
         if($exisiting = Request::param('exisiting')){
           $meeting = new Meeting($exisiting);
-          foreach($this->use as $candidate) $meeting->candidates = $candidate;
+          foreach($this->use as $id){
+            $candidate = new Candidate($id);
+            $candidate->meeting = $meeting;
+          }
           $candidate = new Candidate($this->use[0]);
           $meeting->job = $candidate->job;
           $this->session->add_message("Candidates have been added to ".$meeting->title. " (".date("d/m/Y H:i", strtotime($meeting->date_start) ) .")");
           $this->redirect_to("/admin/meeting/edit/".$meeting->primval."/");
         //creating a new meeting
         }elseif($saved = $this->form->save()){
-          foreach($this->use as $candidate) $saved->candidates = $candidate;
+          foreach($this->use as $id){
+            $candidate = new Candidate($id);
+            $candidate->meeting = $saved;
+          }
           $candidate = new Candidate($this->use[0]);
           $saved->job = $candidate->job;
           $this->session->add_message("Meeting has been created.");
