c946ddeb8897b05e30e1746bac6fa59a35939531
charlesmarshall <cm56marshall@googlemail.com>
2012-08-01 12:36:25 +0100

    add extra column to flag if they are staff or not. holding function for archive, this will do delete etc when rules are decided

--- lib/model/Candidate.php
+++ lib/model/Candidate.php
@@ -19,6 +19,8 @@ class Candidate extends WaxModel{
     $this->define("application", "ForeignKey", array('target_model'=>"Application", 'export'=>true, 'group'=>'relationships', 'widget'=>'HiddenInput', 'editable'=>false));
     $this->define("meeting", "ForeignKey", array('target_model'=>"Meeting", 'export'=>true, 'group'=>'relationships', 'widget'=>'HiddenInput', 'editable'=>false));
 
+    $this->define("is_staff", "BooleanField", array('editable'=>false, 'default'=>0, 'maxlength'=>2, "widget"=>"SelectInput", "choices"=>array(0=>"No",1=>"Yes")));
+
     $this->define("date_created", "DateTimeField");
     $this->define("date_modified", "DateTimeField");
   }
@@ -34,6 +36,12 @@ class Candidate extends WaxModel{
     $command = '/usr/bin/xvfb-run -a -s "-screen 0 1024x768x16" /usr/bin/wkhtmltopdf --encoding utf-8 -s A4 -T 0mm -B 20mm -L 0mm -R 0mm "'.$server.$permalink.'" '.$file;
     shell_exec($command);
     WaxLog::log('error', '[pdf] '.$command, "pdf");
+  }
+  /**
+   * return false for now as not sure rules for this; is should probably remove application as well
+   */
+  public function archive(){
+    return false;
   }
 }
 ?>
