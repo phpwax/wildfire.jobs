cde046661da8b9ed270fbaa7a24f2489f34a64b7
charlesmarshall <cm56marshall@googlemail.com>
2012-08-02 11:13:11 +0100

    meeting from name

--- lib/model/Meetingnotification.php
+++ lib/model/Meetingnotification.php
@@ -4,19 +4,20 @@ class Meetingnotification extends WaxEmail{
   public static $meeting_invite_subject = "Meeting Invitation";
   public static $meeting_changed_subject = "Meeting Update";
   public static $meeting_cancelled_subject = "Meeting Cancelled";
+  public static $custom_from_name = "Careers";
 
 
   public function meeting_invite($meeting, $candidate, $from, $to, $dev_emails=array()){
-    $this->standard_meeting_email($meeting, $candidate, $from, $to, $dev_emails, Meetingnotification::$meeting_invite_subject);
+    $this->standard_meeting_email($meeting, $candidate, $from, $to, $dev_emails, Meetingnotification::$meeting_invite_subject, Meetingnotification::$custom_from_name);
   }
   public function meeting_changed($meeting, $candidate, $from, $to, $dev_emails=array()){
-    $this->standard_meeting_email($meeting, $candidate, $from, $to, $dev_emails, Meetingnotification::$meeting_changed_subject);
+    $this->standard_meeting_email($meeting, $candidate, $from, $to, $dev_emails, Meetingnotification::$meeting_changed_subject, Meetingnotification::$custom_from_name);
   }
   public function meeting_cancelled($meeting, $candidate, $from, $to, $dev_emails=array()){
-    $this->standard_meeting_email($meeting, $candidate, $from, $to, $dev_emails, Meetingnotification::$meeting_cancelled_subject);
+    $this->standard_meeting_email($meeting, $candidate, $from, $to, $dev_emails, Meetingnotification::$meeting_cancelled_subject, Meetingnotification::$custom_from_name);
   }
 
-  protected function standard_meeting_email($meeting, $candidate, $from, $to, $dev_emails, $subject){
+  protected function standard_meeting_email($meeting, $candidate, $from, $to, $dev_emails, $subject, $from_name="Careers"){
     $this->from = $from;
     $this->add_to_address($candidate->email);
     $all = explode(",", str_replace(";", ",", $to));
