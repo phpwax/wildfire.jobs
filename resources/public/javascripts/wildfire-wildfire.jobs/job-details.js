abdc1e05768ea64d4fc12841800fe011db10e7bb
Charles Marshall <charles@oneblackbear.com>
2013-01-08 13:03:39 +0000

    job details view js

---
+++ resources/public/javascripts/wildfire-wildfire.jobs/job-details.js
@@ -0,0 +1,19 @@
+jQuery(document).ready(function(){
+
+  //whenever a checkbox is triggered update all the buttons to have the correct id in the params
+  jQuery(".generic_cms_block input[type=checkbox]").live("click", function(e){
+    var tab = jQuery(this).parents(".generic_cms_block"),
+          buttons = tab.find(".global_actions a"),
+          inputs = tab.find("input[type=checkbox]"),
+          string = "?"
+          ;
+      inputs.each(function(){
+        var val = jQuery(this).val();
+        if(val) string += "ids[]="+val+"&";
+      });
+      buttons.each(function(){
+        var href = jQuery(this).attr("data-href") + string;
+        jQuery(this).attr("href", href);
+      });
+  });
+});
