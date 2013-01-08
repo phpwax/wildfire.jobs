521b72928b624269e249d1239977c02d96adacaf
Charles Marshall <charles@oneblackbear.com>
2013-01-08 18:09:30 +0000

    renamed

--- lib/model/Candidate.php
+++ lib/model/Candidate.php
@@ -17,7 +17,7 @@ class Candidate extends WaxModel{
     $this->define("postcode", "CharField", array('group'=>'details', 'export'=>true,'label'=>'Postcode <small>(%person_postcode%)</small>'));
     $this->define("gender", "CharField", array('group'=>'details', 'export'=>true,'scaffold'=>true, 'label'=>'Gender <small>(%person_gender%)</small>'));
     $this->define("job", "ForeignKey", array('target_model'=>CONTENT_MODEL, 'scaffold'=>true, 'export'=>true, 'group'=>'relationships', 'widget'=>'HiddenInput'));
-    $this->define("application", "ForeignKey", array('target_model'=>"Application", 'export'=>true, 'group'=>'application_answers', 'widget'=>'HiddenInput'));
+    $this->define("application", "ForeignKey", array('target_model'=>"Application", 'export'=>true, 'group'=>'application', 'widget'=>'HiddenInput'));
     $this->define("meeting", "ForeignKey", array('target_model'=>"Meeting", 'export'=>true, 'group'=>'relationships', 'widget'=>'HiddenInput', 'editable'=>false));
     $this->define("last_meeting", "ForeignKey", array('target_model'=>"Meeting", 'editable'=>false, 'col_name'=>'last_meeting_id'));
 
