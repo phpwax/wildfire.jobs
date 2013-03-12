a1fc135a7393ac2c3e559dd6465823fbdf8bb19c
Charles Marshall <charles@oneblackbear.com>
2013-03-12 11:48:33 +0000

    check if id or not

--- lib/model/History.php
+++ lib/model/History.php
@@ -33,7 +33,7 @@ class History extends WaxModel{
   }
   public static function sent_email($job, $application, $sent_to, $type, $user_id){
     $model = new History;
-    return $model->update_attributes(array('title'=>'Sent notification: '.$type, 'application_id'=>(is_numeric($application)) ? $application : $application->primval, 'content'=>'Sent notification of  '.$type.' to: '. $sent_to, 'job_id'=>$job->primval,'wildfire_user_id'=>$user_id));
+    return $model->update_attributes(array('title'=>'Sent notification: '.$type, 'application_id'=>(is_numeric($application)) ? $application : $application->primval, 'content'=>'Sent notification of  '.$type.' to: '. $sent_to, 'job_id'=>$job->primval,'wildfire_user_id'=>(is_numeric($user_id)) ? $user_id : $user_id->primval  ));
   }
   public static function log($job, $application, $user_id, $title, $notes=false){
     $model = new History;
