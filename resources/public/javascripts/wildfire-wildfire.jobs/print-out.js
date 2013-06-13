877544b0397651a20f7581d504ed3303a1b59372
Charles Marshall <charles@oneblackbear.com>
2013-06-13 14:00:46 +0100

    test the print stuff

--- resources/public/javascripts/wildfire-wildfire.jobs/print-out.js
+++ resources/public/javascripts/wildfire-wildfire.jobs/print-out.js
@@ -1,9 +1,16 @@
 jQuery(document).ready(function(){
-  var pg_height = 100, so_far = 0;
+  var pg_height = 800, so_far = 0;
+  if(jQuery("#cms-applications").hasClass("print-view-of-page")){
 
     jQuery(".appfield").each(function(){
       var block = jQuery(this), h = block.outerHeight();
-        block.css({"margin-top": 800});
+      if((so_far+h) >= pg_height){
+        block.css({"margin-top": ((pg_height - so_far)+20) });
+        so_far = 20;
+      }
+      so_far+=h;
+
     });
+  }
 
 });
