a33ec1dda66253ec44e2a89dd7a45349c5de000d
Charles Marshall <charles@oneblackbear.com>
2013-06-13 14:14:34 +0100

    sorting out top margin

--- resources/public/javascripts/wildfire-wildfire.jobs/print-out.js
+++ resources/public/javascripts/wildfire-wildfire.jobs/print-out.js
@@ -1,15 +1,21 @@
 jQuery(document).ready(function(){
-  var pg_height = 800, so_far = 0;
+  var pg_height = 800, so_far = 0, pgs=0;
   if(jQuery("#cms-applications").hasClass("print-view-of-page")){
 
     jQuery(".appfield").each(function(){
-      var block = jQuery(this), h = block.outerHeight();
-      if((so_far+h) >= pg_height){
-        block.css({"margin-top": ((pg_height - so_far)+20) });
-        so_far = 20;
+      var block = jQuery(this),
+            h = block.outerHeight(),
+            p = block.offset().top,
+            b = p+h,
+            pg
+            ;
+      pg = b - (pg_height * (pgs+1));
+      //console.log(p+":"+b+":"+pg);
+      //this means it would wrap
+      if(pg > 0){
+        block.css({"page-break-before":"always", "margin-top": 50  });
+        pgs++;
       }
-      so_far+=h;
-
     });
   }
 
