15546e0afe0a0ec314e2b348834a7454e88d0e1d
Charles Marshall <charles@oneblackbear.com>
2013-06-24 15:19:36 +0100

    add rejection reason to the columns

--- lib/model/DeadApplication.php
+++ lib/model/DeadApplication.php
@@ -5,6 +5,8 @@ class DeadApplication extends Application{
   public function setup(){
     parent::setup();
     $this->columns['date_rejected'][1]['scaffold'] = true;
+    $this->columns['rejection_reason'][1]['scaffold'] = true;
+    $this->columns['rejection_reason'][1]['label'] = 'Rejection Reason';
     $this->columns['rejected'][1]['scaffold'] = false;
     $this->columns['deadend'][1]['scaffold'] = false;
     $this->columns['main_telephone'][1]['scaffold'] = false;
