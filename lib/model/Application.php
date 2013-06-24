0c72a423bde0d99f649a38cc351729781b471b1c
Charles Marshall <charles@oneblackbear.com>
2013-06-24 09:21:38 +0100

    change export file name to be persons name & job title

--- lib/model/Application.php
+++ lib/model/Application.php
@@ -75,7 +75,8 @@ class Application extends WaxModel{
   }
 
   public function create_pdf($module_name, $server, $hash, $folder, $user){
-    $file = $folder.$hash."/".$module_name."-".$this->primval.".pdf";
+    $job = $this->job;
+    $file = $folder.$hash."/".$this->first_name. " " . $this->last_name "-".$job->title.".pdf";
     $url = $server."/admin/".$module_name."/edit/".$this->primval."/.print?auth_token=".$user->auth_token;
 
     // $command = '/usr/bin/xvfb-run -a -s "-screen 0 1024x768x16" /usr/bin/wkhtmltopdf --encoding utf-8 -s A4 -T 0mm -B 20mm -L 0mm -R 0mm "'.$server.$permalink.'" '.$file;
