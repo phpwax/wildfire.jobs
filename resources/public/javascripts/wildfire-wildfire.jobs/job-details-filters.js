396a4a1d71d3539e924bf6a047ae57eb9df5092c
Charles Marshall <charles@oneblackbear.com>
2013-10-18 10:10:57 +0100

    allow a secondary filter on to be passed along as well

--- resources/public/javascripts/wildfire-wildfire.jobs/job-details-filters.js
+++ resources/public/javascripts/wildfire-wildfire.jobs/job-details-filters.js
@@ -1,11 +1,11 @@
 jQuery(document).ready(function(){
   var table_filter_timer = false;
-  jQuery(".generic_cms_block input.filter_table").live("keyup", function(e){
-    var input = jQuery(this),
-          parent = jQuery(this).parents(".generic_cms_block"),
+  jQuery(".generic_cms_block input.filter_table, .generic_cms_block .filter_stage").live("keyup change", function(e){
+    var parent = jQuery(this).parents(".generic_cms_block"),
+          input = parent.find(".filter_table"),
+          select = parent.find(".filter_stage"),
           dest = input.attr("data-dest"),
-          val = input.val(),
-          data = {inlinefilter: val}
+          data = {inlinefilter: input.val(), type:select.val()}
           ;
     clearTimeout(table_filter_timer);
     table_filter_timer = setTimeout(function(){
@@ -16,7 +16,7 @@ jQuery(document).ready(function(){
         success:function(res){
           parent.replaceWith(res);
         }
-      })
+      });
     }, 800);
   });
 
