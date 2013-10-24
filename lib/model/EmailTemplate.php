66c69f11d65c9a1e13945d262c026026d1ee9dc3
Charles Marshall <charles@oneblackbear.com>
2013-10-24 13:31:05 +0100

    work on getting a custom layout, as theyve made a bunch of randomly named ones

--- lib/model/EmailTemplate.php
+++ lib/model/EmailTemplate.php
@@ -5,6 +5,7 @@ class EmailTemplate extends WaxModel{
     parent::setup();
     $this->columns['id'][1]['widget'] = 'HiddenInput';
     $this->define("title", "CharField", array('scaffold'=>true, 'label'=>'Internal title'));
+
     $this->define("subject", "CharField", array('scaffold'=>true));
     $this->define("from_email", "CharField", array('scaffold'=>true));
     $this->define("from_name", "CharField", array('scaffold'=>true));
@@ -13,7 +14,7 @@ class EmailTemplate extends WaxModel{
     $this->define("date_modified", "DateTimeField", array('export'=>true, 'scaffold'=>true, "editable"=>false));
     $this->define("date_created", "DateTimeField", array('export'=>true, "editable"=>false));
     $this->define("media", "ManyToManyField", array('target_model'=>"WildfireMedia", "eager_loading"=>true, "join_model_class"=>"WildfireOrderedTagJoin", "join_order"=>"join_order", 'group'=>'media', 'module'=>'media'));
-
+    $this->define("pdf_view", "CharField", array('label'=>'Custom PDF','choices'=>self::pdf_view_choices(), 'widget'=>'SelectInput' ));
   }
 
   public function before_save(){
@@ -21,6 +22,14 @@ class EmailTemplate extends WaxModel{
     if(!$this->date_created) $this->date_created = date("Y-m-d H:i:s");
     $this->date_modified = date("Y-m-d H:i:s");
     $this->content = stripslashes($this->content);
+  }
+  public static function pdf_view_choices(){
+    $data = array(''=>'-- select --');
+    foreach(glob(VIEW_DIR."admin/extras/-*.print") as $f){
+      $base = basename($f, ".print");
+      $data[$base] = Inflections::humanize($base);
+    }
+    return $data;
   }
 
 
