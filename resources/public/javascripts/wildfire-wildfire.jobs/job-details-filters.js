a0f4355920b40ecbdeacc8f33db54a5ff7bc148a
Michal Noskovic <michal@oneblackbear.com>
2019-02-18 15:23:30 +0000

    Submit pagination filters

--- resources/public/javascripts/wildfire-wildfire.jobs/job-details-filters.js
+++ resources/public/javascripts/wildfire-wildfire.jobs/job-details-filters.js
@@ -1,12 +1,12 @@
 jQuery(document).ready(function(){
   var table_filter_timer = false;
-  jQuery(".generic_cms_block input.filter_table, .generic_cms_block .filter_stage").live("keyup change", function(e){
+  jQuery(".generic_cms_block input.filter_table, .generic_cms_block .filter_stage, .generic_cms_block .filter_page").live("keyup change", function(e){
     var parent = jQuery(this).parents(".generic_cms_block"),
           input = parent.find(".filter_table"),
           select = parent.find(".filter_stage"),
+          page = parent.find(".filter_page"),
           dest = input.attr("data-dest"),
-          data = {inlinefilter: input.val(), type:select.val()}
-          ;
+          data = { inlinefilter: input.val(), type: select.val(), page: page.val() };
     clearTimeout(table_filter_timer);
     table_filter_timer = setTimeout(function(){
       input.addClass("loading");
